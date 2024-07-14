b87f8f75def15f785c20bcbb95b5103db29d8374
Orel Yehudai <103596170+OreLL162@users.noreply.github.com>
2024-07-14 21:45:23 +0300

    Update developer_routes.js

--- backend/routes/developer_routes.js
+++ backend/routes/developer_routes.js
@@ -3,12 +3,12 @@
 //2. Netanel Zohar, id: 318970340
 
 import Express from 'express';
-import { createNewDev, getDev } from '../controllers/developer_controller.js';
+import { createDeveloper, getDevelopers } from '../controllers/developer_controller.js';
 
 const developerRouter = Express.Router();
 
-developerRouter.get('/about', getDev);
+developerRouter.get('/about', getDevelopers);
 
-developerRouter.post('/createdeveloper', createNewDev);
+developerRouter.post('/createdeveloper', createDeveloper);
 
 export default developerRouter;
